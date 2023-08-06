de060eb9050a4f63166d6d9cf2d77f7816667799
jianqiugao <985040320@qq.com>
2023-08-06 22:50:41 +0800

    update:init proj

--- 多臂老虎机/beta分布.py
+++ 多臂老虎机/beta分布.py
@@ -12,7 +12,7 @@ a2, b2 = 4, 12 # 正偏态分布
 a3, b3 = 12, 4 # 负偏态分布
 a4, b4 = 2, 2 # 对称分布
 a5, b5 = 5, 5 # 双峰分布
-res = np.random.beta(a2,b2) # 每次都不一样
+res = np.random.beta(a2,b2,1000) # 每次都不一样
 print(res)
 # 计算不同参数下的概率密度函数值
 y1 = beta.pdf(x, a1, b1)
@@ -25,7 +25,8 @@ y3 = beta.pdf(x, a3, b3)
 plt.plot(x, y1, label="Beta(6,8)")
 plt.plot(x, y2, label="Beta(4,12)")
 plt.plot(x, y3, label="Beta(12,4)")
-plt.axvline(res)
+for x in res:
+    plt.axvline(x)
 # plt.plot(x, y4, label="Beta(2,2)")
 # plt.plot(x, y5, label="Beta(2,5)")
 plt.xlabel("x")
