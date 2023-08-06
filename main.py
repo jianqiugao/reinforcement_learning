56dd01898cd9c59d74c4930acb1bd342d63b892a
jianqiugao <985040320@qq.com>
2023-08-06 20:01:18 +0800

    update:init proj

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+# 实现一个拉杆为10的多臂老虎机
+import numpy as np
+import matplotlib.pyplot as plt
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+class BernoulliBandit:
+    def __init__(self, k):
+        self.probs = np.random.uniform(size=k)
+        self.best_idx = np.argmax(self.probs)
+        self.k = k
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    def step(self, k):
+        if np.random.rand() < self.probs[k]:
+            return 1
+        else:
+            return 0
+np.random.seed(1)
+
+k = 10
+band_it_10_arm = BernoulliBandit(10)
+print(f"随机生成了一个{k}臂老虎机")
